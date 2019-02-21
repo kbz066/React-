d250aa27fb59c2102f814909bd3bbc1b38ad1d85
kbz-home <1529666546@qq.com>
2019-02-21 21:46:47 +0800

    增加中间件

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,22 @@
-import {createStore} from "redux";
+import {createStore,compose,applyMiddleware} from "redux";
 import reducers from './reducers'
+import thunk from "redux-thunk"
 
+
+const composeEnhancers =
+  typeof window === 'object' &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?   
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+      // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+    }) : compose;
+
+const enhancer = composeEnhancers(
+  applyMiddleware(thunk),
+  // other store enhancers if any
+);
 let store = createStore(
     reducers,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    enhancer
     )
 
 export default store;
